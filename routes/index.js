0f94ff0115108314155c4b28b27c442b1fed0b21
wilcoxadvisors <garrettwilcox40@gmail.com>
2025-03-04 17:47:59 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const accountingRoutes = require('./accounting');
 
-// Add your module routes here (accounting.js, etc.)
+router.use('/accounting', accountingRoutes);
 
 module.exports = router;
