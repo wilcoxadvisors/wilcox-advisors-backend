4b6f37f42d319221016892a27bf4f5af01c8cf4f
wilcoxadvisors <garrettwilcox40@gmail.com>
2025-03-05 15:11:50 -0600

    Create database.js

---
+++ config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const logger = require('../utils/logger');
+
+// Database connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    logger.info('MongoDB connected successfully');
+  } catch (error) {
+    logger.error('MongoDB connection error:', error);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
