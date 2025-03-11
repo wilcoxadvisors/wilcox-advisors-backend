9d845b253ba744b2f864737f3d32607ab31691da
wilcoxadvisors <garrettwilcox40@gmail.com>
2025-03-11 14:26:23 -0500

    Update journalEntries.js

--- routes/journalEntries.js
+++ routes/journalEntries.js
@@ -1,12 +1,28 @@
 // routes/journalEntries.js
 const express = require('express');
 const router = express.Router();
-const { getJournalEntries, createJournalEntry } = require('../controllers/journalEntryController');
-const { validateJournalEntry } = require('../validators/journalEntryValidator');
+const { 
+  getJournalEntries, 
+  getJournalEntryById,
+  createJournalEntry,
+  deleteJournalEntry
+} = require('../controllers/journalEntryController');
+const { 
+  validateJournalEntry,
+  validateJournalEntryId
+} = require('../validators/journalEntryValidator');
 const { auth } = require('../middleware/auth');
 
-// Routes with middleware
+// Get all journal entries with filtering
 router.get('/', auth, getJournalEntries);
+
+// Get a single journal entry by ID
+router.get('/:id', auth, validateJournalEntryId, getJournalEntryById);
+
+// Create a new journal entry
 router.post('/', auth, validateJournalEntry, createJournalEntry);
+
+// Delete (reverse) a journal entry
+router.delete('/:id', auth, validateJournalEntryId, deleteJournalEntry);
 
 module.exports = router;
